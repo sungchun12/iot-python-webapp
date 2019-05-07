45eac69001b1a1eb43c6e6eca2d4d817728d2e77
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-06 23:36:11 -0500

    updated docker runbook

---
+++ docker_commands.sh
@@ -0,0 +1,24 @@
+#build the docker image
+docker build -t dash-demo .
+
+#run it on port 8080 for cloud run even though 8050 is the default port
+#https://cloud.google.com/run/docs/reference/container-contract#port
+docker run -it --rm -p 8080:8080 dash-demo
+
+#name the docker image
+docker tag dash-demo justsung/dash-demo:1.0.0
+
+#login to docker hub
+docker login
+
+#push docker image to docker hub
+docker push justsung/dash-demo:1.0.0
+
+#list of docker images
+docker images
+
+#tag image for google container registry
+docker tag <image-id> gcr.io/<project-id>/<image-name>
+
+#push image to google container registry
+docker push gcr.io/<project-id>/<image-name>
