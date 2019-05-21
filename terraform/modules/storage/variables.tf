942080ba1de3d3232161598f1361fce68eaeb417
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-21 16:18:29 -0500

    raw data store bucket config for iot stream

---
+++ terraform/modules/storage/variables.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  default = "us-central1"
+}
+
+variable "project_id" {
+  default = "iconic-range-220603"
+}
+
+# https://upcloud.com/community/tutorials/terraform-variables/
+
