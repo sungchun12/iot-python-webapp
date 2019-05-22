f02649f4bce8a30496c3441ea30e07d457f2943a
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-22 12:24:37 -0500

    call upon root variables

--- terraform/modules/storage/variables.tf
+++ terraform/modules/storage/variables.tf
@@ -1,10 +1,2 @@
-variable "location" {
-  default = "us-central1"
-}
-
-variable "project_id" {
-  default = "iconic-range-220603"
-}
-
-# https://upcloud.com/community/tutorials/terraform-variables/
-
+variable "project" {}
+variable "location" {}
