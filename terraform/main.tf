19a4942a231041aff98adc4aa087c61b3d1e2585
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-22 16:34:09 -0500

    added data_pipeline module

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,16 @@ module "storage" {
   source  = "./modules/storage"
   version = "0.0.1"
 
-  # pass the root module variables to child storage module
+  # pass the root module variables to child module
+  project  = "${var.project}"
+  location = "${var.location}"
+}
+
+module "data_pipeline" {
+  source  = "./modules/data_pipeline"
+  version = "0.0.1"
+
+  # pass the root module variables to child  module
   project  = "${var.project}"
   location = "${var.location}"
 }
