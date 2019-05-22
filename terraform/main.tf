c9d72011236fe8b98eaa591683c329c06c335d86
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-22 12:23:54 -0500

    added centralized variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,14 @@
+provider "google" {
+  project = "${var.project}"
+  region  = "${var.location}"
+  zone    = "us-central1-c"
+}
+
 module "storage" {
   source  = "./modules/storage"
   version = "0.0.1"
+
+  # pass the root module variables to child storage module
+  project  = "${var.project}"
+  location = "${var.location}"
 }
