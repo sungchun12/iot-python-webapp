b7f11fe7fa6030d836f5376b4bd21370f1812667
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-23 12:34:53 -0500

    added multiple output metadata variables

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -10,3 +10,19 @@ output "dataflow-staging-metadata" {
 output "data-pipeline-topic-metadata" {
   value = "${module.data_pipeline.data-pipeline-topic-metadata}"
 }
+
+output "data-pipeline-dataset-metadata" {
+  value = "${module.data_pipeline.data-pipeline-dataset-metadata}"
+}
+
+output "data-pipeline-table-metadata" {
+  value = "${module.data_pipeline.data-pipeline-table-metadata}"
+}
+
+output "data-pipeline-bigtable-metadata" {
+  value = "${module.data_pipeline.data-pipeline-bigtable-metadata}"
+}
+
+output "data-pipeline-bigtable-table-metadata" {
+  value = "${module.data_pipeline.data-pipeline-bigtable-table-metadata}"
+}
