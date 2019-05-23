87c42c3a1d522f4249361c7ead307cafae0eb06e
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-23 12:36:51 -0500

    added bigquery/bigtable output name vars

--- terraform/modules/data_pipeline/outputs.tf
+++ terraform/modules/data_pipeline/outputs.tf
@@ -1,3 +1,19 @@
 output "data-pipeline-topic-metadata" {
   value = "${google_pubsub_topic.data-pipeline-topic.name}"
 }
+
+output "data-pipeline-dataset-metadata" {
+  value = "${google_bigquery_dataset.iot_dataset.dataset_id}"
+}
+
+output "data-pipeline-table-metadata" {
+  value = "${google_bigquery_table.iot_raw_data.table_id}"
+}
+
+output "data-pipeline-bigtable-metadata" {
+  value = "${google_bigtable_instance.iot-stream-database.name}"
+}
+
+output "data-pipeline-bigtable-table-metadata" {
+  value = "${google_bigtable_table.iot-stream-table.name}"
+}
