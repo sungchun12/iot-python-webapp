23347296f9a19daf4ef157b968a4e38336816873
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-24 18:00:10 -0500

    added network, ssh firewall rule, iot-devs

--- terraform/modules/iot_compute/main.tf
+++ terraform/modules/iot_compute/main.tf
@@ -11,7 +11,7 @@ resource "google_compute_instance" "iot-device-1" {
     }
   }
 
-  // Local SSD disk
+  # Local SSD disk
   scratch_disk {}
 
   network_interface {
@@ -22,12 +22,86 @@ resource "google_compute_instance" "iot-device-1" {
 
   metadata_startup_script = "echo hi > /test.txt"
 
-  service_account {
-    scopes = ["service-157930433863@compute-system.iam.gserviceaccount.com", "compute-ro", "storage-ro"]
-  }
+  # service_account {
+  #   scopes = ["service-157930433863@compute-system.iam.gserviceaccount.com", "compute-ro", "storage-ro"]
+  # }
 }
 
-data "google_compute_network" "demo-network" {
+resource "google_compute_instance" "iot-device-2" {
+  name         = "iot-device-2"
+  machine_type = "n1-standard-1"
+  zone         = "us-east1-b"
+
+  tags = ["demo"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  # Local SSD disk
+  scratch_disk {}
+
+  network_interface {
+    # link creatd vpc network
+    network       = "${google_compute_network.demo-network.self_link}"
+    access_config = {}
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  # service_account {
+  #   scopes = ["service-157930433863@compute-system.iam.gserviceaccount.com", "compute-ro", "storage-ro"]
+  # }
+}
+
+resource "google_compute_instance" "iot-device-3" {
+  name         = "iot-device-3"
+  machine_type = "n1-standard-1"
+  zone         = "europe-west2-a"
+
+  tags = ["demo"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  # Local SSD disk
+  scratch_disk {}
+
+  network_interface {
+    # link creatd vpc network
+    network       = "${google_compute_network.demo-network.self_link}"
+    access_config = {}
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+  # service_account {
+  #   scopes = ["service-157930433863@compute-system.iam.gserviceaccount.com", "compute-ro", "storage-ro"]
+  # }
+}
+
+resource "google_compute_network" "demo-network" {
   name                    = "demo-network"
   auto_create_subnetworks = "true"
+}
+
+resource "google_compute_firewall" "ssh-access-firewall" {
+  name        = "ssh-access-firewall"
+  description = "allow ssh access to VM within the project"
+  network     = "${google_compute_network.demo-network.self_link}"
+  direction   = "INGRESS"
+
+  # need to configure ssh access from a variable IP address I'll specify
+  # IP address must be ignored in git repo
+  # tie the ssh access to a service account and ingress any IP range
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
 }
