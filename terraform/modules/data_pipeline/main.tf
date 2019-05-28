3b42d48804a88566df6e8134de7b82b8154c0541
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-28 13:34:50 -0500

    initial draft of iot registry

--- terraform/modules/data_pipeline/main.tf
+++ terraform/modules/data_pipeline/main.tf
@@ -1,3 +1,32 @@
+resource "google_cloudiot_registry" "iot-device-status" {
+  name = "default-registry"
+
+  event_notification_config = {
+    pubsub_topic_name = "${google_pubsub_topic.default-telemetry.id}"
+  }
+
+  state_notification_config = {
+    pubsub_topic_name = "${google_pubsub_topic.default-devicestatus.id}"
+  }
+
+  http_config = {
+    http_enabled_state = "HTTP_ENABLED"
+  }
+
+  mqtt_config = {
+    mqtt_enabled_state = "MQTT_ENABLED"
+  }
+
+  credentials = [
+    {
+      public_key_certificate = {
+        format      = "X509_CERTIFICATE_PEM"
+        certificate = "${file("rsa_cert.pem")}"
+      }
+    },
+  ]
+}
+
 resource "google_pubsub_topic" "data-pipeline-topic" {
   name    = "data-pipeline-topic"
   project = "${var.project}"
