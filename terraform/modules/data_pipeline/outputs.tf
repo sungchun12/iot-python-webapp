afe936079c6f9fc2a4fbe84e43451f485ce565df
Sung Won Chung <sungwon.chung@slalom.com>
2019-05-29 00:02:46 -0500

    updated for v12 syntax

--- terraform/modules/data_pipeline/outputs.tf
+++ terraform/modules/data_pipeline/outputs.tf
@@ -1,27 +1,28 @@
 output "iot-registry-metadata" {
-  value = "${google_cloudiot_registry.iot-registry.name}"
+  value = google_cloudiot_registry.iot-registry.name
 }
 
 output "data-pipeline-topic-metadata" {
-  value = "${google_pubsub_topic.data-pipeline-topic.name}"
+  value = google_pubsub_topic.data-pipeline-topic.name
 }
 
 output "iot-device-status-metadata" {
-  value = "${google_pubsub_topic.iot-device-status.name}"
+  value = google_pubsub_topic.iot-device-status.name
 }
 
 output "data-pipeline-dataset-metadata" {
-  value = "${google_bigquery_dataset.iot_dataset.dataset_id}"
+  value = google_bigquery_dataset.iot_dataset.dataset_id
 }
 
 output "data-pipeline-table-metadata" {
-  value = "${google_bigquery_table.iot_raw_data.table_id}"
+  value = google_bigquery_table.iot_raw_data.table_id
 }
 
 output "data-pipeline-bigtable-metadata" {
-  value = "${google_bigtable_instance.iot-stream-database.name}"
+  value = google_bigtable_instance.iot-stream-database.name
 }
 
 output "data-pipeline-bigtable-table-metadata" {
-  value = "${google_bigtable_table.iot-stream-table.name}"
+  value = google_bigtable_table.iot-stream-table.name
 }
+
