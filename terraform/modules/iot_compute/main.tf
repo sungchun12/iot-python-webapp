0da4f54e691592cae6937e726b26405c85dda8c2
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-03 16:40:48 -0500

    uncommented service account and scope

--- terraform/modules/iot_compute/main.tf
+++ terraform/modules/iot_compute/main.tf
@@ -86,10 +86,10 @@ resource "google_compute_instance" "iot-device-2" {
   ${file("${path.module}/startup_script.sh")}
   SCRIPT
 
-  # service_account {
-  #   scopes = ["service-157930433863@compute-system.iam.gserviceaccount.com", "compute-ro", "storage-ro"]
-  # }
-}
+  service_account {
+    email  = "demo-service-account@iconic-range-220603.iam.gserviceaccount.com"
+    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+  }
 
 resource "google_compute_instance" "iot-device-3" {
   name = "iot-device-3"
