fd83d372f168d0b29ab8c4009f43b586dec1be20
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-03 16:41:51 -0500

    replaced text block with variable file function

--- terraform/modules/iot_compute/main.tf
+++ terraform/modules/iot_compute/main.tf
@@ -23,34 +23,8 @@ resource "google_compute_instance" "iot-device-1" {
   }
 
   metadata_startup_script = <<SCRIPT
-    #define temporary environment path variables
-    iot_directory="/home/sungwon_chung1/iot"
-    files_directory="/training-data-analyst/quests/iotlab/"
-    demo_directory="$iot_directory$files_directory"
-
-    #update the system information about Debian Linux package repositories
-    sudo apt-get update
-
-    #install in scope packages
-    sudo apt-get install python-pip openssl git git-core -y
-
-    #use pip for needed Python components
-    sudo pip install pyjwt paho-mqtt cryptography
-
-    #make a new directory
-    sudo mkdir $iot_directory
-
-    #add data to analyze
-    cd $iot_directory; git clone https://github.com/GoogleCloudPlatform/training-data-analyst.git
-
-    #create RSA cryptographic keypair
-    cd $demo_directory
-    sudo openssl req -x509 -newkey rsa:2048 -keyout rsa_private.pem \
-    -nodes -out rsa_cert.pem -subj "/CN=unused"
-
-    #download the CA root certificates from pki.google.com to the appropriate directory
-    sudo wget https://pki.google.com/roots.pem
-    SCRIPT
+  ${file("${path.module}/startup_script.sh")}
+  SCRIPT
 
   service_account {
     email = "demo-service-account@iconic-range-220603.iam.gserviceaccount.com"
