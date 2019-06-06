21cb87cc946d143fdf0083ddb60bf1f49f38a740
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-06 14:48:16 -0500

    added more pass through root to child vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,15 @@ module "storage" {
   # version = "0.0.1"
 
   # pass the root module variables to child module
-  project  = var.project
-  location = var.location
+  project             = var.project
+  location            = var.location
+  version_label       = var.version_label
+  raw_bucket_name     = var.raw_bucket_name
+  staging_bucket_name = var.staging_bucket_name
+  storage_class       = var.storage_class
+  versioning_enabled  = var.versioning_enabled
+  main_page_suffix    = var.main_page_suffix
+  not_found_page      = var.not_found_page
 }
 
 module "data_pipeline" {
