ba6c8805f68f8f0c208af60e24470c68251249c6
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-06 14:31:32 -0500

    added root module vars

--- terraform/modules/storage/variables.tf
+++ terraform/modules/storage/variables.tf
@@ -4,3 +4,20 @@ variable "project" {
 variable "location" {
 }
 
+variable "raw_bucket_name" {
+}
+
+variable "staging_bucket_name" {
+}
+
+variable "storage_class" {
+}
+
+variable "versioning_enabled" {
+}
+
+variable "main_page_suffix" {
+}
+
+variable "not_found_page" {
+}
