d05a38ec7f79bfb1fda34bb5f6103223ac99b9f8
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-07 16:14:48 -0500

    added vars to pipeline module

--- terraform/modules/data_pipeline/variables.tf
+++ terraform/modules/data_pipeline/variables.tf
@@ -7,3 +7,41 @@ variable "location" {
 variable "zone" {
 }
 
+variable "version_label" {
+}
+
+variable "iot_registry_name" {
+}
+
+variable "http_config_state" {
+}
+
+variable "mqtt_config_state" {
+}
+
+variable "data_pipeline_topic_name" {
+}
+
+variable "device_status_topic_name" {
+}
+
+variable "dataset_name" {
+}
+variable "dataset_desc" {
+}
+variable "table_name" {
+}
+variable "table_desc" {
+}
+variable "bigtable_db_name" {
+}
+variable "bigtable_db_instance_type" {
+}
+variable "bigtable_db_cluster_name" {
+}
+variable "bigtable_db_storage_type" {
+}
+variable "bigtable_table_name" {
+}
+variable "bigtable_table_split_keys" {
+}
