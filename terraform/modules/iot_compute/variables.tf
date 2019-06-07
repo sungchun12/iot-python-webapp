154738bd12688825c79c84149427370931592ebd
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-07 14:52:22 -0500

    added root module vars for iot compute

--- terraform/modules/iot_compute/variables.tf
+++ terraform/modules/iot_compute/variables.tf
@@ -7,3 +7,43 @@ variable "location" {
 variable "zone" {
 }
 
+variable "device_name_1" {
+}
+
+variable "device_name_1_zone" {
+}
+
+variable "device_name_2" {
+}
+
+variable "device_name_2_zone" {
+}
+
+variable "device_name_3" {
+}
+
+variable "device_name_3_zone" {
+}
+
+variable "machine_type" {
+}
+
+variable "os_image" {
+}
+
+variable "service_account_email" {
+}
+
+variable "service_account_scopes" {
+}
+
+variable "network_name" {
+}
+
+variable "firewall_ssh_name" {
+}
+variable "allow_protocol" {
+}
+
+variable "allow_ports" {
+}
