2a63661fdf5f5d09612abaf75b0df1caf484368c
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-14 12:39:24 -0500

    add var types and organized with docstrings

--- terraform/modules/data_pipeline/variables.tf
+++ terraform/modules/data_pipeline/variables.tf
@@ -1,3 +1,7 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These variables are expected to be passed in by the operator
+# ---------------------------------------------------------------------------------------------------------------------
 variable "project" {
 }
 
@@ -13,77 +17,96 @@ variable "service_account_email" {
 variable "version_label" {
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL MODULE PARAMETERS
+# These variables have defaults, but may be overridden by the operator
+# ---------------------------------------------------------------------------------------------------------------------
 variable "iot_registry_name" {
-  default     = "iot-registry"
   description = "Name of IOT registry to manage devices"
+  type        = string
+  default     = "iot-registry"
 }
 
 variable "http_config_state" {
-  default     = "HTTP_DISABLED"
   description = "Enable or disable http protocol"
+  type        = string
+  default     = "HTTP_DISABLED"
 }
 
 variable "mqtt_config_state" {
-  default     = "MQTT_ENABLED"
   description = "Enable or disable mqtt protocol"
+  type        = string
+  default     = "MQTT_ENABLED"
 }
 
 variable "data_pipeline_topic_name" {
-  default     = "data-pipeline-topic"
   description = "Name of pubsub topic for raw device data streams"
+  type        = string
+  default     = "data-pipeline-topic"
 }
 
 variable "device_status_topic_name" {
-  default     = "iot-device-status"
   description = "Name of pubsub topic for device status streams"
+  type        = string
+  default     = "iot-device-status"
 }
 
 variable "dataset_name" {
-  default     = "iot_dataset"
   description = "Name of BigQuery dataset for data streams"
+  type        = string
+  default     = "iot_dataset"
 }
 
 variable "dataset_desc" {
-  default     = "iot data warehouse"
   description = "Description of BigQuery dataset for data streams"
+  type        = string
+  default     = "iot data warehouse"
 }
 
 variable "table_name" {
-  default     = "iot_raw_data"
   description = "Name of BigQuery table for raw data streams"
+  type        = string
+  default     = "iot_raw_data"
 }
 
 variable "table_desc" {
-  default     = "table that accumulates all raw iot streaming data"
   description = "Description of BigQuery table for raw data streams"
+  type        = string
+  default     = "table that accumulates all raw iot streaming data"
 }
 
 variable "bigtable_db_name" {
-  default     = "iot-stream-database"
   description = "Name of BigTable instance for raw data streams"
+  type        = string
+  default     = "iot-stream-database"
 }
 
 variable "bigtable_db_instance_type" {
-  default     = "DEVELOPMENT"
   description = "Type of instance"
+  type        = string
+  default     = "DEVELOPMENT"
 }
 
 variable "bigtable_db_cluster_name" {
-  default     = "iot-stream-database-cluster"
   description = "Name of BigTable cluster for raw data streams"
+  type        = string
+  default     = "iot-stream-database-cluster"
 }
 
 variable "bigtable_db_storage_type" {
-  default     = "SSD"
   description = "BigTable instance storage type"
+  type        = string
+  default     = "SSD"
 }
 
 variable "bigtable_table_name" {
-  default     = "iot-stream-table"
   description = "BigTable instance storage type"
+  type        = string
+  default     = "iot-stream-table"
 }
 
 variable "bigtable_table_split_keys" {
-  default     = ["a", "b", "c"]
   description = "define table partition keys"
+  type        = list
+  default     = ["a", "b", "c"]
 }
