43b1da0eacbb4cd6b1ae58992cbbe1d833ad5470
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-14 12:25:08 -0500

    formatted with docstrings and var types

--- terraform/modules/iot_compute/variables.tf
+++ terraform/modules/iot_compute/variables.tf
@@ -1,3 +1,7 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# These variables are expected to be passed in by the operator
+# ---------------------------------------------------------------------------------------------------------------------
 variable "project" {
 }
 
@@ -13,72 +17,91 @@ variable "service_account_email" {
 variable "version_label" {
 }
 
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL MODULE PARAMETERS
+# These variables have defaults, but may be overridden by the operator
+# ---------------------------------------------------------------------------------------------------------------------
 variable "device_name_1" {
-  default     = "iot-device-1"
   description = "name of the VM to simulate an IOT device"
+  type        = string
+  default     = "iot-device-1"
 }
 
 variable "device_name_1_zone" {
-  default     = "us-east1-c"
   description = "zone for IOT device"
+  type        = string
+  default     = "us-east1-c"
 }
 
 variable "device_name_2" {
-  default     = "iot-device-2"
   description = "name of the VM to simulate an IOT device"
+  type        = string
+  default     = "iot-device-2"
 }
 
 variable "device_name_2_zone" {
-  default     = "us-east1-b"
   description = "zone for IOT device"
+  type        = string
+  default     = "us-east1-b"
+
 }
 
 variable "device_name_3" {
-  default     = "iot-device-3"
   description = "name of the VM to simulate an IOT device"
+  type        = string
+  default     = "iot-device-3"
 }
 
 variable "device_name_3_zone" {
-  default     = "europe-west2-a"
   description = "zone for IOT device"
+  type        = string
+  default     = "europe-west2-a"
 }
 
 variable "machine_type" {
-  default     = "n1-standard-1"
   description = "we'll go with a small, standard VM"
+  type        = string
+  default     = "n1-standard-1"
 }
 
 variable "os_image" {
-  default     = "debian-cloud/debian-9"
   description = "Typically a linux image, debian is lightweight"
+  type        = string
+  default     = "debian-cloud/debian-9"
 }
 
 variable "service_account_scopes" {
-  default     = ["https://www.googleapis.com/auth/cloud-platform"]
   description = "GCP access rights for VM which will override service account permissions if more limited"
+  type        = list
+  default     = ["https://www.googleapis.com/auth/cloud-platform"]
 }
 
 variable "network_name" {
-  default     = "demo-network"
   description = "Name of the VPC network for VMs to reside within"
+  type        = string
+  default     = "demo-network"
 }
 
 variable "firewall_ssh_name" {
-  default     = "ssh-access-firewall"
   description = "Name of the firewall allowing ssh access"
+  type        = string
+  default     = "ssh-access-firewall"
 }
 
 variable "firewall_ssh_description" {
-  default     = "allow ssh access to VM within the project"
   description = "description for firewall"
+  type        = string
+  default     = "allow ssh access to VM within the project"
 }
 
 variable "allow_protocol" {
-  default     = "tcp"
   description = "type of ssh protocol: rdp or tcp"
+  type        = string
+  default     = "tcp"
 }
 
 variable "allow_ports" {
-  default     = ["22"]
   description = "port to ssh into"
+  type        = list
+  default     = ["22"]
 }
