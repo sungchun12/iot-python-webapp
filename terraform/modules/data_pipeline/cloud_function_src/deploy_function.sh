f9eae69f555d20e2056440ed0a2b3183230599ea
Sung Won Chung <sungwon.chung@slalom.com>
2019-06-19 14:05:14 -0500

    more scratch pad code

--- terraform/modules/data_pipeline/cloud_function_src/deploy_function.sh
+++ terraform/modules/data_pipeline/cloud_function_src/deploy_function.sh
@@ -8,6 +8,9 @@ export GOOGLE_APPLICATION_CREDENTIALS="C:\Users\sungwon.chung\Desktop\repos\serv
 
 set GOOGLE_APPLICATION_CREDENTIALS=C:\Users\sungwon.chung\Desktop\repos\serverless_dash_repo\serverless_dash\terraform\service_account.json
 
+set GCLOUD_PROJECT=iconic-range-220603
+set BIGTABLE_CLUSTER=iot-stream-database
+
 python main.py ^
 iconic-range-220603 ^
 iot-stream-database
