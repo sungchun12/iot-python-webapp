47aa75d722bd810bf9a6578cb7751195d28803de
sungwon_chung <sungwon_chung@apple.com>
2019-09-30 15:00:03 -0500

    add in comments for backlog

--- src/cloud_run_iam_policy.sh
+++ src/cloud_run_iam_policy.sh
@@ -5,5 +5,3 @@ gcloud beta run services add-iam-policy-binding tf-dash-cloud-run-demo \
 --member="allUsers" \
 --role="roles/run.invoker" \
 --region="us-central1"
-
-gcloud beta run services update tf-dash-cloud-run-demo --allow-unauthenticated
