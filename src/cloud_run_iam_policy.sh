8d4321fdc95103389767da0ad8a9eca584a6312b
sungwon_chung <sungwon_chung@apple.com>
2019-09-30 15:38:26 -0500

    scratch pad

--- src/cloud_run_iam_policy.sh
+++ src/cloud_run_iam_policy.sh
@@ -5,3 +5,11 @@ gcloud beta run services add-iam-policy-binding tf-dash-cloud-run-demo \
 --member="allUsers" \
 --role="roles/run.invoker" \
 --region="us-central1"
+
+export GCLOUD_PROJECT_NAME="iconic-range-220603"
+export BIGTABLE_CLUSTER="iot-stream-database"
+export TABLE_NAME="iot-stream-table"
+export CLOUD_REGION="us-central1"
+export IOT_REGISTRY="iot-registry"
+export ROW_FILTER=2
+export GOOGLE_APPLICATION_CREDENTIALS="/Users/sungwon.chung/Desktop/repos/serverless-dash-webapp/terraform/service_account.json"
