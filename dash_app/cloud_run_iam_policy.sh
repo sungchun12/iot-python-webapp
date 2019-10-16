6c8ae832c537bc1a67c0251f119a4c2bcafef574
sungwon_chung <sungwon_chung@apple.com>
2019-10-16 15:27:50 -0500

    gcloud commands to encrypt/decrypt

--- dash_app/cloud_run_iam_policy.sh
+++ dash_app/cloud_run_iam_policy.sh
@@ -14,6 +14,50 @@ export IOT_REGISTRY="iot-registry"
 export ROW_FILTER=2
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/sungwon.chung/Desktop/repos/serverless-dash-webapp/tf_modules/service_account.json"
 
+# steps to encrypt private key for cloud run
+# https://stackoverflow.com/questions/48602546/google-cloud-functions-how-to-securely-store-service-account-private-key-when
+# TODO: can add the below scripts to a single script with file argument
+# create bucket for encrypted service account json private key
+gsutil mb -c standard -l us-central1 -p iconic-range-220603 -b on gs://secure-bucket-cloud-run
+
+# delete the bucket as needed
+gsutil rm -r gs://secure-bucket-cloud-run
+
+# create a keyring
+gcloud kms keyrings create cloud-run-keyring \
+--location=global
+
+#create a key
+gcloud kms keys create iot-python-webapp-key \
+--purpose=encryption \
+--location=global \
+--keyring=cloud-run-keyring \
+--protection-level=software
+
+#encrypt file using KMS
+# https://cloud.google.com/sdk/gcloud/reference/kms/encrypt
+gcloud kms encrypt \
+--key=iot-python-webapp-key \
+--keyring=cloud-run-keyring \
+--location=global \
+--plaintext-file=/Users/sungwon.chung/Desktop/repos/serverless-dash-webapp/tf_modules/service_account.json \
+--ciphertext-file=ciphertext_file.enc
+
+# Copy encrypted file to cloud storage bucket
+gsutil cp ciphertext_file.enc gs://secure-bucket-cloud-run
+
+# list objects in bucket
+gsutil ls -r gs://secure-bucket-cloud-run
+
+#decrypt file using KMS
+gcloud kms decrypt \
+--key=iot-python-webapp-key \
+--keyring=cloud-run-keyring \
+--location=global \
+--plaintext-file=testFile.json \
+--ciphertext-file=gs://secure-bucket-cloud-run/ciphertext_file
+
+
 
 #tag image for google container registry
 # docker tag <image-id> gcr.io/<project-id>/<image-name>
