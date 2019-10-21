891f66f1d3c3ef619e8d9c47647f28c88d3f2fbe
sungwon_chung <sungwon_chung@apple.com>
2019-10-21 16:38:51 -0500

    add more ad hoc commands

--- dash_app/cloud_run_iam_policy.sh
+++ dash_app/cloud_run_iam_policy.sh
@@ -12,9 +12,13 @@ export TABLE_NAME="iot-stream-table"
 export CLOUD_REGION="us-central1"
 export IOT_REGISTRY="iot-registry"
 export ROW_FILTER=2
-export KEY_RING_ID="cloud-run-keyring"
+export KEY_RING_ID="cloud-run-keyring-v5"
 export CRYPTO_KEY_ID="iot-python-webapp-key"
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/sungwon.chung/Desktop/repos/serverless-dash-webapp/service_account.json"
+export GOOGLE_APP_CREDENTIALS="CiQA50Ad3WaKLGR3xlIgKzNz7caas4ceMEtfnote43POiHAQkV8SQACvmJFehfFm/gdai0gBG5HsVXdOyz+h13UtdIHMLS0LzP/atcZnsdtSKyBP2qV2mgZwnqzOs8EM7GID09p3YB4="
+
+import base64
+base64.b64encode(bytes(GOOGLE_APP_CREDENTIALS, 'utf-8'))
 
 # steps to encrypt private key for cloud run
 # https://stackoverflow.com/questions/48602546/google-cloud-functions-how-to-securely-store-service-account-private-key-when
@@ -61,15 +65,18 @@ gcloud kms decrypt \
 --plaintext-file=testFile.json \
 --ciphertext-file=gs://secure-bucket-cloud-run/ciphertext_file
 
-
+# Traceback (most recent call last): File "/usr/local/lib/python3.7/site-packages/gunicorn/arbiter.py", line 583, in spawn_worker worker.init_process() File "/usr/local/lib/python3.7/site-packages/gunicorn/workers/gthread.py", line 104, in init_process super(ThreadWorker, self).init_process() File "/usr/local/lib/python3.7/site-packages/gunicorn/workers/base.py", line 129, in init_process self.load_wsgi() File "/usr/local/lib/python3.7/site-packages/gunicorn/workers/base.py", line 138, in load_wsgi self.wsgi = self.app.wsgi() File "/usr/local/lib/python3.7/site-packages/gunicorn/app/base.py", line 67, in wsgi self.callable = self.load() File "/usr/local/lib/python3.7/site-packages/gunicorn/app/wsgiapp.py", line 52, in load return self.load_wsgiapp() File "/usr/local/lib/python3.7/site-packages/gunicorn/app/wsgiapp.py", line 41, in load_wsgiapp return util.import_app(self.app_uri) File "/usr/local/lib/python3.7/site-packages/gunicorn/util.py", line 350, in import_app __import__(module) File "/app/app.py", line 18, in <module> devices_list = cbt_data_generator.get_device_names() File "/app/access_iot_data.py", line 85, in get_device_names self.decrpyt_symmetric_text(self.__service_account_ciphertext), File "/app/access_iot_data.py", line 68, in decrpyt_symmetric_text response = client.decrypt(name, ciphertext) File "/usr/local/lib/python3.7/site-packages/google/cloud/kms_v1/gapic/key_management_service_client.py", line 1816, in decrypt request, retry=retry, timeout=timeout, metadata=metadata File "/usr/local/lib/python3.7/site-packages/google/api_core/gapic_v1/method.py", line 143, in __call__ return wrapped_func(*args, **kwargs) File "/usr/local/lib/python3.7/site-packages/google/api_core/retry.py", line 273, in retry_wrapped_func on_error=on_error, File "/usr/local/lib/python3.7/site-packages/google/api_core/retry.py", line 182, in retry_target return target() File "/usr/local/lib/python3.7/site-packages/google/api_core/timeout.py", line 214, in func_with_timeout return func(*args, **kwargs) File "/usr/local/lib/python3.7/site-packages/google/api_core/grpc_helpers.py", line 59, in error_remapped_callable six.raise_from(exceptions.from_grpc_error(exc), exc) File "<string>", line 3, in raise_from google.api_core.exceptions.InvalidArgument: 400 Decryption failed: the ciphertext is invalid.
 
 #tag image for google container registry
 # docker tag <image-id> gcr.io/<project-id>/<image-name>
-docker tag 43a265500959 gcr.io/iconic-range-220603/dash-demo-v2
+# docker tag 5427d9091df4 gcr.io/iconic-range-220603/dash-cloudrun-demo
 
 #push image to google container registry
 # docker push gcr.io/<project-id>/<image-name>
-docker push gcr.io/iconic-range-220603/dash-demo-v2
+# docker push gcr.io/iconic-range-220603/dash-cloudrun-demo
+
+# ad hoc push to container registry from dockerfile
+gcloud builds submit --tag gcr.io/iconic-range-220603/dash-cloudrun-demo
 
 # cloud run example
 gcloud beta run deploy iot-python-webapp --platform managed --image gcr.io/iconic-range-220603/dash-demo-v2:latest --region us-central1 --allow-unauthenticated --set-env-vars=GCLOUD_PROJECT_NAME="iconic-range-220603",BIGTABLE_CLUSTER="iot-stream-database",TABLE_NAME="iot-stream-table",CLOUD_REGION="us-central1",IOT_REGISTRY="iot-registry",ROW_FILTER=2
