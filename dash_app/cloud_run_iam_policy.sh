4dd6007502eb2cc2cd20a92c7feb5335beb02e29
sungwon_chung <sungwon_chung@apple.com>
2019-10-24 10:33:44 -0500

    move scripts to separate location

--- dash_app/cloud_run_iam_policy.sh
+++ dash_app/cloud_run_iam_policy.sh
@@ -63,72 +63,12 @@ gcloud kms decrypt \
 
 #add parameters as part of deployment
 # startup_script.sh for your_username="realsww123" and projectid=iot-python-webapp-demo
-# variables.tf for project_id and service_account_emailfor terraform deployment
+# variables.tf for project_id and service_account_email for terraform deployment
 # backend.tf for bucket name
 # use a random number generator similar to the startup script to create dynamic crypto key rings
+# shell script for project id 
 
-# all through cloud shell
 
-# create bucket for encrypted service account json private key
-PROJECT_ID=iot-python-webapp-demo
-gsutil mb gs://$PROJECT_ID-secure-bucket-tfstate
-# gsutil mb -c standard -l us-central1 -p iot-python-webapp-demo -b on gs://$PROJECT_ID-secure-bucket-tfstate
-gsutil versioning set on gs://$PROJECT_ID-secure-bucket-tfstate
-
-#create a service account
-gcloud iam service-accounts create demo-service-account \
---description "service account used to launch terraform locally" \
---display-name "demo-service-account"
-
-# list service accounts to verify creation
-gcloud iam service-accounts list
-
-#enable newly created service account based on what's listed
-gcloud beta iam service-accounts enable \
-demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com
-
-# add editor encryptor, and security admin role, so it has permissions to launch many kinds of terraform resources
-gcloud projects add-iam-policy-binding iot-python-webapp-demo \
---member serviceAccount:demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com \
---role roles/editor
-
-gcloud projects add-iam-policy-binding iot-python-webapp-demo \
---member serviceAccount:demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com \
---role roles/cloudkms.cryptoKeyEncrypter
-
-gcloud projects add-iam-policy-binding iot-python-webapp-demo \
---member serviceAccount:demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com \
---role roles/iam.securityAdmin
-
-gcloud projects add-iam-policy-binding iot-python-webapp-demo \
---member serviceAccount:demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com \
---role roles/cloudkms.admin
-
-# check if roles updated
-# note: may not be accurate even though console shows the update
-gcloud iam service-accounts get-iam-policy \
-demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com
-
-# clone git repo
-git clone https://github.com/sungchun12/iot-python-webapp.git
-
-# download the service account key where gcloud lives
-gcloud iam service-accounts keys create ~/iot-python-webapp/service_account.json \
---iam-account demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com
-
-# download locally
-gcloud iam service-accounts keys create ~/Desktop/repos/serverless-dash-webapp/service_account.json \
---iam-account demo-service-account@iot-python-webapp-demo.iam.gserviceaccount.com
-
-# move the service account key to the repo
-# note: it'll be ignored
-# mv service_account.json ~/iot-python-webapp
-
-# change directory to tf_modules
-cd tf_modules/
-
-# ad hoc push to container registry from dockerfile at root directory
-gcloud builds submit --tag gcr.io/iot-python-webapp-demo/dash-cloudrun-demo
 
 # terraform deployment
 terraform init
