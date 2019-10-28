1b6e384610aeecf5ba6c4a9220a3e31b5c855aed
sungchun12 <sungwonchung3@gmail.com>
2019-10-27 23:43:24 -0500

    reverse merge commit

--- src/iot_manager_sample.sh
+++
@@ -1,6 +0,0 @@
-python iot_manager.py \
---project_id=iconic-range-220603 \
---cloud_region=us-central1 \
---registry_id=iot-registry \
---service_account_json=/Users/sungwon.chung/Desktop/repos/serverless-dash-webapp/terraform/service_account.json \
-list_registries
--- tf_modules/modules/secrets_manager/variables.tf
+++ tf_modules/modules/secrets_manager/variables.tf
@@ -32,3 +32,4 @@ variable "webapp_key_name" {
 
 }
 
+
